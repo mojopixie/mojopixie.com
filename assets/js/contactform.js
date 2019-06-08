5eff5a63c0abd5e700072cc94d6d8b263ae55e7e
Michael Fine <mfine@fineonline.com>
2019-06-08 11:51:42 -0700

    Added 'phone' case to contactform.js after weird comment issue

--- assets/js/contactform.js
+++ assets/js/contactform.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($) {
         var f = $(this).find('.form-group'),
             ferror = false,
             emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
-            phoneExp = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+            phoneExp = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/i;
 
         f.children('input').each(function() { // run all inputs
 
@@ -35,12 +35,6 @@ jQuery(document).ready(function($) {
                             ferror = ierror = true;
                         }
                         break;
-
-                    case 'phone':
-                        if (!phoneExp.test(i.val())) {
-                            ferror = ierror = true;
-                        }
-                        break;
     
                     case 'email':
                         if (!emailExp.test(i.val())) {
@@ -48,11 +42,17 @@ jQuery(document).ready(function($) {
                         }
                         break;
 
-                    case 'checked':
-                        if (!i.is(':checked')) {
+                    case 'phone':
+                        if (!phoneExp.test(i.val())) {
                             ferror = ierror = true;
                         }
                         break;
+
+                    // case 'checked':
+                    //     if (!i.is(':checked')) {
+                    //         ferror = ierror = true;
+                    //     }
+                    //     break;
 
                     case 'regexp':
                         exp = new RegExp(exp);
