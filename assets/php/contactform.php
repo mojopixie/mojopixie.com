021cf233a4ab05de993204c2cadb4320b1ea44fb
mojopixie <susanmbernard@gmail.com>
2019-06-09 12:33:41 -0400

    trying to fix contact form again

--- assets/php/contactform.php
+++ assets/php/contactform.php
@@ -1,7 +1,7 @@
 <?php
 /*
   PHP contact form script
-  Version: 1.1  
+  Version: 1.1
 */
 
 /***************** Configuration *****************/
@@ -13,8 +13,6 @@
   $subject_title = "mojopixie.com's Contact Form message:";
   $name_title = "Name:";
   $email_title = "Email:";
-  //added phone
-  // $phone_title = "Phone:";
   $message_title = "Message:";
 
   // Error messages
@@ -22,63 +20,60 @@
   $contact_error_email = "Please enter valid email!";
   $contact_error_subject = "Too short; please enter valid subject!";
   $contact_error_message = "Too short; please enter valid subject!";
-  //added phone
-  // $contact_error_phone = "Please enter valid phone!";
-
 
 /********** Do not edit from the below line ***********/
 
-if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-  die('Sorry Request must be Ajax POST');
-}
-
-if(isset($_POST)) {
-
-  $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-  $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-  $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
-
-  if(!$contact_email_to || $contact_email_to == 'contact@example.com') {
-    die('The contact form receiving email address is not configured!');
+  if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+    die('Sorry Request must be Ajax POST');
   }
 
-  if(strlen($name)<3){
-    die($contact_error_name);
+  if(isset($_POST)) {
+
+    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+
+    if(!$contact_email_to || $contact_email_to == 'contact@example.com') {
+      die('The contact form receiving email address is not configured!');
+    }
+
+    if(strlen($name)<3){
+      die($contact_error_name);
+    }
+
+    if(!$email){
+      die($contact_error_email);
+    }
+
+    if(strlen($subject)<3){
+      die($contact_error_subject);
+    }
+
+    if(strlen($message)<3){
+      die($contact_error_message);
+    }
+
+    if(!isset($contact_email_from)) {
+      $contact_email_from = "contactform@" . @preg_replace('/^www\./','', $_SERVER['SERVER_NAME']);
+    }
+
+    $headers = 'From: ' . $name . ' <' . $contact_email_from . '>' . PHP_EOL;
+    $headers .= 'Reply-To: ' . $email . PHP_EOL;
+    $headers .= 'MIME-Version: 1.0' . PHP_EOL;
+    $headers .= 'Content-Type: text/html; charset=UTF-8' . PHP_EOL;
+    $headers .= 'X-Mailer: PHP/' . phpversion();
+
+    $message_content = '<strong>' . $name_title . '</strong> ' . $name . '<br>';
+    $message_content .= '<strong>' . $email_title . '</strong> ' . $email . '<br>';
+    $message_content .= '<strong>' . $message_title . '</strong> ' . nl2br($message);
+
+    $sendemail = mail($contact_email_to, $subject_title . ' ' . $subject, $message_content, $headers);
+
+    if( $sendemail ) {
+      echo 'OK';
+    } else {
+      echo 'Could not send mail! Please check your PHP mail configuration.';
+    }
   }
-
-  if(!$email){
-    die($contact_error_email);
-  }
-
-  if(strlen($subject)<3){
-    die($contact_error_subject);
-  }
-
-  if(strlen($message)<3){
-    die($contact_error_message);
-  }
-
-  if(!isset($contact_email_from)) {
-    $contact_email_from = "contactform@" . @preg_replace('/^www\./','', $_SERVER['SERVER_NAME']);
-  }
-
-  $headers = 'From: ' . $name . ' <' . $contact_email_from . '>' . PHP_EOL;
-  $headers .= 'Reply-To: ' . $email . PHP_EOL;
-  $headers .= 'MIME-Version: 1.0' . PHP_EOL;
-  $headers .= 'Content-Type: text/html; charset=UTF-8' . PHP_EOL;
-  $headers .= 'X-Mailer: PHP/' . phpversion();
-
-  $message_content = '<strong>' . $name_title . '</strong> ' . $name . '<br>';
-  $message_content .= '<strong>' . $email_title . '</strong> ' . $email . '<br>';
-  $message_content .= '<strong>' . $message_title . '</strong> ' . nl2br($message);
-
-  $sendemail = mail($contact_email_to, $subject_title . ' ' . $subject, $message_content, $headers);
-
-  if( $sendemail ) {
-    echo 'OK';
-  } else {
-    echo 'Could not send mail! Please check your PHP mail configuration.';
-  }
-}
 ?>
