38020a9f90a0ea3c24e6f43d05c54c7a49ddf9ac
SB <susanmbernard@gmail.com>
2019-06-19 14:00:45 -0400

    master to match

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,38 +1,45 @@
+/*=========== TABLE OF CONTENTS ===========
+###Preloader
+1. Smooth scrolling 
+2. superfish: Initiate on nav menu
+  // Mobile Navigation
+  // Stick the header at top on scroll
+  // Counting numbers
+  // Tooltip & popovers
+  // Background image via data tag
+  // Background image via data tag
+  // jQuery counterUp
+3. Scroll Top link
+4. owl carousel
+
+
 $(document).ready(function() {
+    //Preloader
+    $(window).on("load", function() {
+        preloaderFadeOutTime = 500
 
-    /*=========== TABLE OF CONTENTS ===========
-    ###Preloader
-    1. Smooth scrolling 
-    2. superfish: Initiate on nav menu
-      // Mobile Navigation
-      // Stick the header at top on scroll
-      // Counting numbers
-      // Tooltip & popovers
-      // Background image via data tag
-      // Background image via data tag
-      // jQuery counterUp
-    3. Scroll Top link
-    4. owl carousel
-
-
-    $(document).ready(function() {
-        //Preloader
-        $(window).on("load", function() {
-            preloaderFadeOutTime = 500
-
-            function hidePreloader() {
-                var preloader = $('.spinner-wrapper');
-                preloader.fadeOut(preloaderFadeOutTime);
-            }
-            hidePreloader();
-        });
+        function hidePreloader() {
+            var preloader = $('.spinner-wrapper');
+            preloader.fadeOut(preloaderFadeOutTime);
+        }
+        hidePreloader();
     });
+});
 
 
-    ======================================*/
+======================================*/
 
 
-    // 1. Smooth scrolling
+$(window).on('load', function() { // makes sure the whole site is loaded 
+    $('#status').fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(350).css({ 'overflow': 'visible' });
+})
+
+
+
+// 1. Smooth scrolling
+$(document).ready(function() {
     $(function() {
         $('a[href*="#"]:not([href="#"])').click(function() {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
